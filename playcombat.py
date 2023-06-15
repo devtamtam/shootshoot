6a3fb5b1b549885e7f6cae88c21824b43aef5a8d
devtamtam <115604773+devtamtam@users.noreply.github.com>
2023-06-16 08:43:03 +0900

    enabled to shoot at the same time from each player

--- playcombat.py
+++ playcombat.py
@@ -19,9 +19,14 @@ box2_y = screen_height // 2 - box_height // 2
 box2_speed = 5
 
 # Bullet attributes
-bullet_width, bullet_height = 10, 5
-bullet_speed = 10
-bullet_state = "ready"
+bullet1_width,bullet1_height = 10, 5
+bullet1_speed = 10
+bullet1_state = "ready"
+
+bullet2_width,bullet2_height = 10, 5
+bullet2_speed = 10
+bullet2_state = "ready"
+
 
 # Game loop
 running = True
@@ -30,15 +35,30 @@ clock = pygame.time.Clock()
 def box(x, y, color):
     pygame.draw.rect(screen, color, (x, y, box_width, box_height))
 
-def fire_bullet(x, y, direction):
-    global bullet_state
-    bullet_state = "fire"
-    pygame.draw.rect(screen, (255, 255, 255), (x, y + box_height // 2 - bullet_height // 2, bullet_width, bullet_height))
+def fire_bullet1(x, y, direction):
+    global bullet1_state
+    bullet1_state = "fire"
+    pygame.draw.rect(screen, (255, 255, 255), (x, y + box_height // 2 - bullet1_height // 2, bullet1_width, bullet1_height))
     if direction == "left":
-        return -bullet_speed
+        return -bullet1_speed
     elif direction == "right":
-        return bullet_speed
+        return bullet1_speed
     return 0
+
+
+
+
+def fire_bullet2(x, y, direction):
+    global bullet2_state
+    bullet2_state = "fire"
+    pygame.draw.rect(screen, (255, 255, 255), (x, y + box_height // 2 - bullet2_height // 2, bullet2_width, bullet2_height))
+    if direction == "left":
+        return -bullet2_speed
+    elif direction == "right":
+        return bullet2_speed
+    return 0
+
+
 
 while running:
     clock.tick(60)  # Set the frame rate
@@ -48,16 +68,22 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and bullet_state == "ready":
-                bullet_x = box1_x + box_width
-                bullet_y = box1_y
-                bullet_state = "fire"
-                bullet_direction = "right"
-            if event.key == pygame.K_p and bullet_state == "ready":
-                bullet_x = box2_x
-                bullet_y = box2_y
-                bullet_state = "fire"
-                bullet_direction = "left"
+            if event.key == pygame.K_SPACE and bullet1_state == "ready":
+                bullet1_x = box1_x + box_width
+                bullet1_y = box1_y
+                bullet1_state = "fire"
+                bullet1_direction = "right"
+            if event.key == pygame.K_p and bullet2_state == "ready":
+                bullet2_x = box2_x
+                bullet2_y = box2_y
+                bullet2_state = "fire"
+                bullet2_direction = "left"
+    
+    #handle evnt2   
+    
+    
+    
+    
     
     # Handle box 2 movement
     keys = pygame.key.get_pressed()
@@ -80,19 +106,33 @@ while running:
     if keys[pygame.K_s]:
         box1_y += box1_speed
     
-    # Update bullet position
-    if bullet_state == "fire":
-        if bullet_x > screen_width or bullet_x < 0:
-            bullet_state = "ready"
+    # Update bullet11 position
+    if bullet1_state == "fire":
+        if bullet1_x > screen_width or bullet1_x < 0:
+            bullet1_state = "ready"
         else:
-            if bullet_direction == "left" and bullet_y >= box1_y and bullet_y <= box1_y + box_height and bullet_x >= box1_x and bullet_x <= box1_x + box_width:
-                bullet_state = "ready"
-            elif bullet_direction == "right" and bullet_y >= box2_y and bullet_y <= box2_y + box_height and bullet_x >= box2_x and bullet_x <= box2_x + box_width:
-                bullet_state = "ready"
+            if bullet1_direction == "left" and bullet1_y >= box1_y and bullet1_y <= box1_y + box_height and bullet1_x >= box1_x and bullet1_x <= box1_x + box_width:
+                bullet1_state = "ready"
+            elif bullet1_direction == "right" and bullet1_y >= box2_y and bullet1_y <= box2_y + box_height and bullet1_x >= box2_x and bullet1_x <= box2_x + box_width:
+                bullet1_state = "ready"
             
-            if bullet_state == "fire":
-                bullet_x += fire_bullet(bullet_x, bullet_y, bullet_direction)
+            if bullet1_state == "fire":
+                bullet1_x += fire_bullet1(bullet1_x, bullet1_y, bullet1_direction)
     
+    #update bullet2 position
+    if bullet2_state == "fire":
+        if bullet2_x > screen_width or bullet2_x < 0:
+            bullet2_state = "ready"
+        else:
+            if bullet2_direction == "left" and bullet2_y >= box1_y and bullet2_y <= box1_y + box_height and bullet2_x >= box1_x and bullet2_x <= box1_x + box_width:
+                bullet2_state = "ready"
+            elif bullet2_direction == "right" and bullet2_y >= box2_y and bullet2_y <= box2_y + box_height and bullet2_x >= box2_x and bullet2_x <= box2_x + box_width:
+                bullet2_state = "ready"
+            
+            if bullet2_state == "fire":
+                bullet2_x += fire_bullet2(bullet2_x, bullet2_y, bullet2_direction)
+    
+
     # Clear the screen
     screen.fill((0, 0, 0))
     
@@ -101,8 +141,13 @@ while running:
     box(box2_x, box2_y, (0, 255, 0))
     
     # Draw the bullet
-    if bullet_state == "fire":
-        pygame.draw.rect(screen, (255, 255, 255), (bullet_x, bullet_y + box_height // 2 - bullet_height // 2, bullet_width, bullet_height))
+    if bullet1_state == "fire":
+       pygame.draw.rect(screen, (255, 255, 255), (bullet1_x, bullet1_y + box_height // 2 - bullet1_height // 2, bullet1_width, bullet1_height))
+
+    if bullet2_state == "fire":
+       pygame.draw.rect(screen, (255, 255, 255), (bullet2_x, bullet2_y + box_height // 2 - bullet2_height // 2, bullet2_width, bullet2_height))
+    
+
     
     # Update the display
     pygame.display.flip()
@@ -203,6 +248,9 @@ while running:
             
             if bullet_state == "fire":
                 bullet_x += fire_bullet(bullet_x, bullet_y, bullet_direction)
+    
+
+    
     
     # Clear the screen
     screen.fill((0, 0, 0))
